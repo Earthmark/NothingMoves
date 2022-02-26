2918e804dcbf7fb3bd411756c8cf6284a87f6d5f
Earthmark (Daniel Miller) <earthmark.miller@gmail.com>
2022-02-25 18:31:27 -0800

    Added the files from the suggested CI setup

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![windows_subsystem = "windows"]
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("textures/icon.png"),
+        ..Default::default()
+    });
+}
